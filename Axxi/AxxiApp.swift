daf8b7b3b90d2d5ca62472908eacb2f06d094eb9
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-04-07 21:22:50 +0530

    Initial Commit

---
+++ Axxi/AxxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxxiApp.swift
+//  Axxi
+//
+//  Created by ajoalex on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AxxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
